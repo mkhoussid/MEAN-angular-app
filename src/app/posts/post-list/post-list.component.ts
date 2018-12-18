316d88970b9c55d0866db1c626693bcd3fe6f1bd
Michael <khmike@hotmail.ca>
2018-12-18 10:45:55 -0500

    Added basic structural directives to posts accordion

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -6,9 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./post-list.component.css']
 })
 export class PostListComponent {
-  post = [
-    { title: 'First Post', content: 'This is the first (1st) post' },
-    { title: 'Second Post', content: 'This is the second (2nd) post' },
-    { title: 'Third Post', content: 'This is the third (3rd) post' }
-  ];
+  // posts = [
+  //   { title: 'First Post', content: 'This is the first (1st) post' },
+  //   { title: 'Second Post', content: 'This is the second (2nd) post' },
+  //   { title: 'Third Post', content: 'This is the third (3rd) post' }
+  // ];
+
+  posts = [];
 }
