e37d0f4534a655736a562cdfa818c35e993da108
Unknown <khmike@hotmail.ca>
2018-12-21 13:45:16 -0500

    Added getPosts route
    /api/posts

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -16,7 +16,7 @@ export class PostListComponent implements OnInit, OnDestroy {
   constructor(public postsService: PostsService) {}
 
   ngOnInit() {
-    this.posts = this.postsService.getPosts();
+    this.postsService.getPosts();
     this.PostsSub = this.postsService
       .getPostUpdateListener()
       .subscribe((posts: Post[]) => {
