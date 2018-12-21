0ae609bf4581f9e47f6b1495c7111b2ef1f74db7
Unknown <khmike@hotmail.ca>
2018-12-21 12:34:26 -0500

    Tested fetching dummy data

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('a');
-  next();
-});
-app.use((req, res, next) => {
-  res.send('second');
+app.use('/api/posts', (req, res, next) => {
+  const posts = [
+    {
+      id: 'fa336h',
+      title: 'first server side post',
+      content: 'coming from the server'
+    },
+    {
+      id: 'qa736h',
+      title: 'second server side post',
+      content: 'coming from the server@'
+    },
+    {
+      id: 'mj764q',
+      title: 'third server side post',
+      content: 'coming from the server!'
+    }
+  ];
+  res.status(200).json({
+    message: 'Posts fetched successfully',
+    posts: posts
+  });
 });
 
 module.exports = app;
