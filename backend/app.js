3bd0c1c50fe94e1e5a6f3ef2978c145abe398321
Unknown <khmike@hotmail.ca>
2018-12-22 12:17:23 -0500

    Added displaying response data (from mongodb)

--- backend/app.js
+++ backend/app.js
@@ -48,26 +48,11 @@ app.post('/api/post', (req, res, next) => {
 });
 
 app.get('/api/posts', (req, res, next) => {
-  const posts = [
-    {
-      id: 'fa336h',
-      title: 'first server side post',
-      content: 'coming from the server'
-    },
-    {
-      id: 'qa736h',
-      title: 'second server side post',
-      content: 'coming from the server@'
-    },
-    {
-      id: 'mj764q',
-      title: 'third server side post',
-      content: 'coming from the server!'
-    }
-  ];
-  res.status(200).json({
-    message: 'Posts fetched successfully',
-    posts: posts
+  Post.find().then(documents => {
+    res.status(200).json({
+      message: 'Posts fetched successfully',
+      posts: documents //you can do res.map() because mongodb has '__id', but our structure has 'id'
+    });
   });
 });
 
