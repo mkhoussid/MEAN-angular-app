2bc40d439dfb350103ae118ddcc80431a8f49792
Unknown <khmike@hotmail.ca>
2018-12-22 15:17:00 -0500

    Finalized edit feature

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -37,9 +37,10 @@ export class PostsService {
   }
 
   getPost(id: string) {
-    return {
-      ...this.posts.find(p => p.id === id)
-    };
+    // return { ...this.posts.find(p => p.id === id) };
+    return this.http.get<{ _id: string; title: string; content: string }>(
+      'http://localhost:3000/api/posts/' + id
+    );
   }
 
   addPost(title: string, content: string) {
