5079486963d9fb2ceff76b92d4516a5695b77382
Unknown <khmike@hotmail.ca>
2018-12-23 16:01:29 -0500

    Added User ID to Posts

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -3,7 +3,8 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next) => {
   try {
     const token = req.headers.authorization.split(' ')[1];
-    jwt.verify(token, 'you_better_be_studying');
+    const decodedToken = jwt.verify(token, 'you_better_be_studying');
+    req.userData = { email: decodedToken.email, userId: decodedToken.userId };
     next();
   } catch (error) {
     res.status(401).json({
