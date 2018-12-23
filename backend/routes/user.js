bc083c02448d22484b3db8df37ae8886d2e2135d
Unknown <khmike@hotmail.ca>
2018-12-23 11:53:32 -0500

    Create new user on request + bcrypt (backend)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,38 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 
 const router = express.Router();
 
-router.post('/signup', (req, res, next) => {});
+router.post('/signup', (req, res, next) => {
+  bcrypt.hash(req.body.password, 10).then(hash => {
+    const user = new user({
+      email: req.body.email,
+      password: hash
+    });
+    user
+      .save()
+      .then(result => {
+        res.status(201).json({
+          message: `User with email ${req.body.email} created`,
+          result: result
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: `Email ${req.body.email} already exists`,
+          error: err
+        });
+      });
+  });
+});
 
 module.exports = router;
+
+// store like this if you dont want to hash / encrypt passwords
+
+// router.post('/signup', (req, res, next) => {
+//   const user = new user({
+//     email: req.body.email,
+//     password: req.body.password
+//   })
+// });
