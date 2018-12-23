64fd8f8e47edb18cf2a3174af3e62d6c622cec64
Unknown <khmike@hotmail.ca>
2018-12-23 09:59:54 -0500

    Pagination logic

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -89,7 +89,13 @@ router.put(
 );
 
 router.get('', (req, res, next) => {
-  Post.find().then(documents => {
+  const pageSize = +req.query.pagesize; //params are strings by default, we need numeric values, so add '+'
+  const currentPage = +req.query.page;
+  const postQuery = Post.find();
+  if (pageSize && currentPage) {
+    postQuery.skip(pageSize * (currentPage - 1)).limit(pageSize);
+  }
+  postQuery.then(documents => {
     res.status(200).json({
       message: 'Posts fetched successfully',
       posts: documents //you can do res.map() because mongodb has '__id', but our structure has 'id'
