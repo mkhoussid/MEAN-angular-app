cf7578c1b4203ac144be84212cfa153822412e90
Unknown <khmike@hotmail.ca>
2018-12-23 12:11:31 -0500

    Create new user (frontend 1/2)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 
+const User = require('../models/user');
+
 const router = express.Router();
 
 router.post('/signup', (req, res, next) => {
   bcrypt.hash(req.body.password, 10).then(hash => {
-    const user = new user({
+    console.log('\n\n\n\n\n\n\n\n\nentered\n\n\n\n\n\n\n\n\n\n');
+    const user = new User({
       email: req.body.email,
       password: hash
     });
