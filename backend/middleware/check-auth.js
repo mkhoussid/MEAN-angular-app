4b547546eeab74e560023f0d07f762b0522b43f8
Unknown <khmike@hotmail.ca>
2018-12-24 10:13:43 -0500

    Added more meaningful errors

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -8,7 +8,7 @@ module.exports = (req, res, next) => {
     next();
   } catch (error) {
     res.status(401).json({
-      message: 'Token incorrect',
+      message: 'Token not authenticated',
       error: error
     });
   }
--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -43,24 +43,33 @@ router.post(
       imagePath: url + '/images/' + req.file.filename,
       creator: req.userData.userId
     });
-    post.save().then(createdPost => {
-      res.status(201).json({
-        message: 'Post added successfully',
-        post: {
-          id: createdPost._id,
-          title: createdPost.title,
-          content: createdPost.content,
-          imagePath: createdPost.imagePath
+    post
+      .save()
+      .then(createdPost => {
+        res.status(201).json({
+          message: 'Post added successfully',
+          post: {
+            id: createdPost._id,
+            title: createdPost.title,
+            content: createdPost.content,
+            imagePath: createdPost.imagePath
 
-          /*
+            /*
             or you could always do,
             post: {
               ...createdPost,
               id: createdPost._id,
           */
-        }
+          }
+        });
+      })
+      .catch(err => {
+        res.status(500).json({
+          message:
+            'Creating post ' + req.body.title.substring(0, 10) + ' failed',
+          error: err
+        });
       });
-    });
   }
 );
 
@@ -82,18 +91,24 @@ router.put(
       imagePath: imagePath,
       creator: req.userData.userId
     });
-    Post.updateOne(
-      { _id: req.params.id, creator: req.userData.userId },
-      post
-    ).then(result => {
-      if (result.n > 0) {
-        res
-          .status(200)
-          .json({ message: `Successfully updated ${req.params.id}` });
-      } else {
-        res.status(401).json({ message: `Failed to update ${req.params.id}` });
-      }
-    });
+    Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post)
+      .then(result => {
+        if (result.n > 0) {
+          res
+            .status(200)
+            .json({ message: `Successfully updated ${req.params.id}` });
+        } else {
+          res
+            .status(401)
+            .json({ message: `Failed to update PostID: ${req.params.id}` });
+        }
+      })
+      .catch(err => {
+        res.status(500).json({
+          message: `Failed to update PostID: ${req.params.id}`,
+          error: err
+        });
+      });
   }
 );
 
@@ -116,24 +131,37 @@ router.get('', (req, res, next) => {
         posts: fetchedPosts, //you can do res.map() because mongodb has '__id', but our structure has 'id'
         maxPosts: count //send this to front end so pagination knows
       });
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Failed to fetch posts',
+        error: err
+      });
     });
 });
 
 router.get('/:id', (req, res, next) => {
-  Post.findById(req.params.id).then(post => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res
-        .status(404)
-        .json({ message: `Post with ID: ${req.params.id} not found!` });
-    }
-  });
+  Post.findById(req.params.id)
+    .then(post => {
+      if (post) {
+        res.status(200).json(post);
+      } else {
+        res
+          .status(404)
+          .json({ message: `Post with ID: ${req.params.id} not found!` });
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Failed to fetch post with ID: ' + req.params.id,
+        error: err
+      });
+    });
 });
 
 router.delete('/:id', checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }).then(
-    result => {
+  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId })
+    .then(result => {
       if (result.n > 0) {
         res
           .status(200)
@@ -143,8 +171,13 @@ router.delete('/:id', checkAuth, (req, res, next) => {
           message: `Failed to delete Post with ID: ${req.params.id}!`
         });
       }
-    }
-  );
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: 'Something went wrong!',
+        error: err
+      });
+    });
 });
 
 module.exports = router;
