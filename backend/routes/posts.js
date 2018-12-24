27256ffa76c5556cf052d1537563043b67810ca2
Unknown <khmike@hotmail.ca>
2018-12-23 21:23:52 -0500

    Implemented authorization with userId

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -79,10 +79,11 @@ router.put(
       _id: req.body.id,
       title: req.body.title,
       content: req.body.content,
-      imagePath: imagePath
+      imagePath: imagePath,
+      creator: req.userData.userId
     });
     Post.updateOne(
-      { _id: req.params.id, createdPost: req.userData.userId },
+      { _id: req.params.id, creator: req.userData.userId },
       post
     ).then(result => {
       if (result.n > 0) {
--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -56,6 +56,7 @@ export class PostsService {
       title: string;
       content: string;
       imagePath: string;
+      creator: string;
     }>('http://localhost:3000/api/posts/' + id);
   }
 
@@ -88,7 +89,8 @@ export class PostsService {
         id: id,
         title: title,
         content: content,
-        imagePath: image
+        imagePath: image,
+        creator: null
       };
     }
     this.http
