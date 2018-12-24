e19e5b95185a40c19557bb32c58f18f9e805b339
Unknown <khmike@hotmail.ca>
2018-12-24 11:49:43 -0500

    Optimizations: setting globals (frontend)

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -4,6 +4,9 @@ import { AuthData } from './auth-data.model';
 import { Subject } from 'rxjs';
 import { Router } from '@angular/router';
 
+import { environment } from '../../environments/environment';
+const BACKEND_URL = environment.apiUrl + '/user/';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -46,7 +49,7 @@ export class AuthService {
       email: email,
       password: password
     };
-    this.http.post('http://localhost:3000/api/user/signup', authData).subscribe(
+    this.http.post(BACKEND_URL + '/signup', authData).subscribe(
       () => {
         this.router.navigate(['/login']);
       },
@@ -64,7 +67,7 @@ export class AuthService {
     };
     this.http
       .post<{ token: string; expiresIn: number; userId: string }>(
-        'http://localhost:3000/api/user/login',
+        BACKEND_URL + '/login',
         authData
       )
       .subscribe(
